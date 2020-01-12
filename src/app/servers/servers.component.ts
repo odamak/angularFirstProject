41e27b8b2b4212e878a5763b807c6cbd28f92687
Omar Damak <omardamak1@gmail.com>
2020-01-12 21:29:19 +0100

    Use ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -21,8 +22,10 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created! Name is '+ this.serverName;
     this.serverCreated = true;
+    this .servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is '+ this.serverName;
+    
   }
 
   onUpdateServerName(event: Event) {
