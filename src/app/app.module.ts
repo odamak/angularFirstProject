e943348873c8f4f2a3b5a72172e22f619aeba687
Omar Damak <omardamak1@gmail.com>
2020-01-12 19:38:52 +0100

    Add user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
     ServersComponent,
     WarningAlertComponent,
     SuccessAlertComponent,
+    UserInfoComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-info/user-info.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-info',
+  templateUrl: './user-info.component.html',
+  styleUrls: ['./user-info.component.css']
+})
+export class UserInfoComponent implements OnInit {
+
+  userName = 'userTestName'
+
+  constructor() { 
+  }
+
+  ngOnInit() {
+  }
+
+  onResetUser(){
+    this.userName = '';
+  }
+
+}
